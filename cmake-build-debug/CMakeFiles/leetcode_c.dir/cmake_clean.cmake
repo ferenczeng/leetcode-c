eb084e29363457273827f833b71c88cb50bd849a
Ferenc Zeng <ferenc.zeng@gmail.com>
2021-08-14 10:35:44 +0800

    init project

---
+++ cmake-build-debug/CMakeFiles/leetcode_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode_c.dir/main.c.o"
+  "leetcode_c"
+  "leetcode_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/leetcode_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
